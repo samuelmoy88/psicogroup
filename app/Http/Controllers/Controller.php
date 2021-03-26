4db8e1aa6b71a9ed74ccb846186ce01391fed788
unknown <samuel.moy@mac.com>
2021-03-26 08:31:17 +0100

    Add a new method to return either json or redirect depending whether the request is ajax

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,8 +6,18 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Support\Facades\Request;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    protected function makeResponse($route, $message = [], $code = 200)
+    {
+        if (Request::ajax()) {
+            return response()->json(['status' => $code]);
+        }
+
+        return redirect(route($route))->with(...$message);
+    }
 }
