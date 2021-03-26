d723e67cc58b09c8f11ddf6c137e94509ebc93f2
unknown <samuel.moy@mac.com>
2021-03-26 08:37:25 +0100

    SortableJS implementation: call a specific closure after the element is sorted

--- resources/js/admin.js
+++ resources/js/admin.js
@@ -4,8 +4,6 @@ import 'alpinejs';
 
 require('./admin-alpine');
 
-
-
 let form = document.querySelector('form');
 
 if (form) {
@@ -35,3 +33,56 @@ if (form) {
     }
 
 }
+
+async function xhr(url, params) {
+    const response = await fetch(url, {
+        method: params.method,
+        mode: 'cors',
+        cache: 'no-cache',
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json, text-plain, */*",
+            "X-Requested-With": "XMLHttpRequest",
+            "X-CSRF-TOKEN": document.querySelector('meta[name="csrf-token"]').getAttribute('content'),
+        },
+        body: JSON.stringify(params.body)
+    });
+
+    return response.json();
+}
+
+
+// Complete SortableJS (with all plugins)
+import Sortable from 'sortablejs/modular/sortable.complete.esm.js';
+
+let sortableEl = document.getElementById('sortable');
+
+Sortable.create(sortableEl, {
+    handle: '.handle',
+    animation: 150,
+    swapThreshold: 0.70,
+    direction: 'vertical',
+    onChange: function (event) {
+        reorderModels();
+    }
+});
+
+function reorderModels() {
+
+    sortableEl.children;
+    let order = 1;
+    for (let tr of sortableEl.children) {
+        xhr(tr.dataset.route, {
+            method: 'PUT',
+            body: {
+                id: tr.dataset.id,
+                order: order,
+                title: tr.dataset.title
+            }
+        }).then();
+        order++;
+    }
+}
+
+
+
