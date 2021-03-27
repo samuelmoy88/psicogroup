a382b34371529dfeb1ceb21b024c56070e592a87
unknown <samuel.moy@mac.com>
2021-03-27 15:25:50 +0100

    Auth ES labels

---
+++ resources/lang/es/auth.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'failed' => 'Estas credenciales no existen en nuestros registros.',
+    'password' => 'Credenciales inválidas.',
+    'throttle' => 'Demasiados intentos de ingreso. Por favor intente otra vez en :seconds segundos.',
+
+];
