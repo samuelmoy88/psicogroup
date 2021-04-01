2d41f52f40dba704b7305570a066e7409b134f8e
unknown <samuel.moy@mac.com>
2021-04-01 13:00:58 +0200

    Passwords spanish labels

---
+++ resources/lang/es/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'reset' => 'Tu contraseña ha sido reiniciada.',
+    'sent' => 'Te hemos enviado un enlace para reiniciar tu contraseña.',
+    'throttled' => 'Por favor, espera un momento antes de reintentar.',
+    'token' => 'Este token de contraseña no es válido.',
+    'user' => "No hemos podido encontrar un usuario con ese email.",
+
+];
