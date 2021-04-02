275d9521f02e4edb76d711fd5e921f465f599e61
unknown <samuel.moy@mac.com>
2021-04-02 14:39:55 +0200

    Fix some styling

--- resources/views/components/alert-danger.blade.php
+++ resources/views/components/alert-danger.blade.php
@@ -1,6 +1,6 @@
 <div class="w-full bg-white mt-2 mb-3 rounded px-6 bg-red-500">
     <div class="flex items-center py-4">
-        <i class="fas fa-times border-2 border-white px-2 py-2 rounded-full fill-current font-light text-white"></i>
+        <i class="fas fa-times border-white px-2 py-2 fill-current text-white"></i>
         <div class="ml-5">
             <p class="text-white my-0">{{ $slot }}</p>
         </div>
