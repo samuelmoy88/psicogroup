4199aa233f82acfae421726d7302f08abf9c46e4
unknown <samuel.moy@mac.com>
2021-04-02 14:30:38 +0200

    New alert danger component

---
+++ resources/views/components/alert-danger.blade.php
@@ -0,0 +1,8 @@
+<div class="w-full bg-white mt-2 mb-3 rounded px-6 bg-red-500">
+    <div class="flex items-center py-4">
+        <i class="fas fa-times border-2 border-white px-2 py-2 rounded-full fill-current font-light text-white"></i>
+        <div class="ml-5">
+            <p class="text-white my-0">{{ $slot }}</p>
+        </div>
+    </div>
+</div>
