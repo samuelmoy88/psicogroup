4a83940d082ade19840c4c182be4178930306e65
unknown <samuel.moy@mac.com>
2021-04-02 14:34:23 +0200

    Cast banned_until date

--- app/Models/User.php
+++ app/Models/User.php
@@ -52,6 +52,7 @@ class User extends Authenticatable implements MustVerifyEmail
      */
     protected $casts = [
         'email_verified_at' => 'datetime',
+        'banned_until' => 'datetime'
     ];
 
     protected $with = ['profile'];
