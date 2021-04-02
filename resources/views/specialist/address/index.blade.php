761869af199e527a13bdd647d71ecaac5009c8b9
unknown <samuel.moy@mac.com>
2021-04-02 12:54:45 +0200

    Show the address' clinic name

--- resources/views/specialist/address/index.blade.php
+++ resources/views/specialist/address/index.blade.php
@@ -13,7 +13,8 @@
                 <li class="address mb-4 border border-gray-300 rounded p-2">
                     <div class="flex justify-between">
                         <div class="flex flex-col justify-between">
-                            <a class="text-blue-500 cursor-pointer" href="{{ route('specialist.addresses.edit', ['uuid' => auth()->user()->uuid, 'address' => $address->id]) }}">{{ $address->title }}</a>
+                            <a class="text-blue-500 cursor-pointer" href="{{ route('specialist.addresses.edit', ['uuid' => auth()->user()->uuid, 'address' => $address->id]) }}"
+                                >{{ $address->clinic_name ? $address->clinic_name . ' - ' : '' }} {{ $address->title }}</a>
                             <address>
                                 @if($address->street) {{ $address->street }}<br/> @endif
                                 @if($address->city) {{ $address->city }} @endif @if($address->zip_code) {{ $address->zip_code }} @endif
