f753d93c6b901309bea52886433a19507d56c0cd
unknown <samuel.moy@mac.com>
2021-04-07 08:34:45 +0200

    Agrandar un poco las miniaturas

--- resources/views/admin/specialists/index.blade.php
+++ resources/views/admin/specialists/index.blade.php
@@ -23,7 +23,7 @@
                             <tr class="text-gray-700 dark:text-gray-400">
                                 <td class="px-4 py-3 truncate" title="{{ $specialist->first_name }}">
                                     <div class="flex items-center text-sm">
-                                        <div class="relative hidden w-8 h-8 mr-3 rounded-full md:block">
+                                        <div class="relative hidden w-12 h-12 mr-3 rounded-full md:block">
                                             <img class="object-cover w-full h-full rounded-full" src="{{ $specialist->profile->avatarPath }}" alt="" loading="lazy">
                                             <div class="absolute inset-0 rounded-full shadow-inner" aria-hidden="true"></div>
                                         </div>
